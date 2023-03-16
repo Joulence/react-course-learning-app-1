29073ff8d9f39f01ca0956343c35f6744f7952a2
Joulence <4erviken@gmail.com>
2023-03-16 19:46:02 +0200

    Improving JSX code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,16 @@
 function ExpenseItem() {
     return (
-        <h2>Expense Item!</h2>
+        <div>
+            <div>
+                March 28th 2021
+            </div>
+            <div>
+                <h2>Car Insurence</h2>
+            </div>
+            <div>
+                $294.67
+            </div>
+        </div>
     );
 }
 

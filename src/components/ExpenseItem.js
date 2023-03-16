f67b28339c73afd9ec15384a36e7fa2b7d08c42d
Joulence <4erviken@gmail.com>
2023-03-16 19:54:13 +0200

    Added css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
-            <div>
-                March 28th 2021
-            </div>
-            <div>
+        <div className='expense-item'>
+            <div>March 28th 2021</div>
+            <div className='expense-item__description'>
                 <h2>Car Insurence</h2>
-            </div>
-            <div>
-                $294.67
+                <div className='expense-item__price'>$294.67</div>
             </div>
         </div>
     );

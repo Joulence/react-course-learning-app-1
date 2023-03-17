11a68b61151a0a8e72216e8877b2a711d0469391
Joulence <4erviken@gmail.com>
2023-03-17 19:48:34 +0200

    Rewriting in arrow method function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -8,7 +8,7 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -26,7 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-        <Expenses items = {expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 }
